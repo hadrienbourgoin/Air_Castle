27b6d837303cd91eef06c70c900b1d4397c8bc22
Hadrien Bourgoin <hadrienbourgoin@gmail.com>
2023-08-21 16:21:08 +0200

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: "castles#index"
+
+  # Rails.application.routes.draw do
+  #   devise_for :users, controllers: {
+  #     sessions: 'users/sessions'
+  #   }
+  # end
 end

9eebbcac29e0bce597817b2cd6ff35c20955cc20
Hadrien Bourgoin <hadrienbourgoin@gmail.com>
2023-08-21 17:46:04 +0200

    avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+User.destroy_all
+20.times do
+  password = Faker::Internet.password
+  User.create(email: Faker::Internet.email,
+              password: password,
+              password_confirmation: password,
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              username: Faker::Internet.username(specifier: 6..10))
+end

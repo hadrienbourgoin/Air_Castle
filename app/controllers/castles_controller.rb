25f34f1cd3831ac66cd3073757a9211d58d030c3
Hadrien Bourgoin <hadrienbourgoin@gmail.com>
2023-08-22 16:34:57 +0200

    castles_index

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,23 +1,26 @@
 class CastlesController < ApplicationController
   before_action :set_user, only: %i[new create]
-  
+
   def index
     @castles = Castle.all
   end
-  def show 
+
+  def show
     @castle = Castle.find(params[:id])
   end
+
   def new
     @castle = Castle.new
   end
+
   def create
     @castle = Castle.new(castle_params)
     @castle.user = @user
-   
+
     if @castle.save
       redirect_to castle_path(@castle)
     else
-      render :new, status :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -27,11 +30,16 @@ class CastlesController < ApplicationController
     redirect_to castles_path
   end
 
+  def mine
+  end
+
   private
+
   def castle_params
     params.require(:castle).permit(:name, :address, :description, :price, :photo)
   end
 
   def set_user
     @user = user.find(params[:user_id])
+  end
 end

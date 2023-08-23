f39550a739a07bc6f4e595d800d8f1970eaf8fd3
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-08-23 14:39:43 +0200

    booking day 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+    @booking = Booking.where(user: current_user).find(params[:id])
+  end
+  
+  def new
+    @user = User.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.where(user: current_user).new(booking_params)
+    @booking.save!
+    redirect_to bookings_path(current_user)
+  end
+
+  def destroy
+    @booking = Booking.current_user
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+  
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:date_begin, :date_end)
+  end
+
+  
 end

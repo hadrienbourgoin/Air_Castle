f07afd1354372cc8cf4e4cbcc06065ee659aa2fb
Hadrien Bourgoin <hadrienbourgoin@gmail.com>
2023-08-25 14:53:18 +0200

    heroku deploy

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -9,4 +9,10 @@ class Castle < ApplicationRecord
   validates :photos, limit: { min: 0, max: 5 }
 
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include AlgoliaSearch
+
+  algoliasearch do
+    attributes :name, :address, :description
+  end
 end
